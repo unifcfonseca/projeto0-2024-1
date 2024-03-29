af5e62c52f1370b0d21aaa600b244b1a7ef6b5da
Leonardo Anjoletto Ferreira <2048533-anjoletto@users.noreply.gitlab.com>
2024-03-29 02:09:23 -0300

    feat: funções com mensagens para teste

---
+++ tarefas.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "tarefas.h"
+
+ERROS criar(Tarefa tarefas[], int *pos){
+    printf("Funcao de criar tarefa\n");
+    return OK;
+}
+
+ERROS deletar(Tarefa tarefas[], int *pos){
+    printf("Funcao de deletar tarefa\n");
+    return OK;
+}
+
+ERROS listar(Tarefa tarefas[], int *pos){
+    printf("Funcao de listar tarefas\n");
+    return OK;
+}
+
+ERROS salvar(Tarefa tarefas[], int *pos){
+    printf("Funcao de salvar tarefas\n");
+    return OK;
+}
+
+ERROS carregar(Tarefa tarefas[], int *pos){
+    printf("Funcao de carregar tarefas\n");
+    return OK;
+}
