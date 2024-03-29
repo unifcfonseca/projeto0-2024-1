5a06833464b8f7858060f156352714a9d91143ad
Leonardo Anjoletto Ferreira <2048533-anjoletto@users.noreply.gitlab.com>
2024-03-29 02:38:04 -0300

    feat: implementação de deletar tarefa

--- tarefas.c
+++ tarefas.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 #include "tarefas.h"
 
 ERROS criar(Tarefa tarefas[], int *pos){
@@ -20,7 +21,26 @@ ERROS criar(Tarefa tarefas[], int *pos){
 }
 
 ERROS deletar(Tarefa tarefas[], int *pos){
-    printf("Funcao de deletar tarefa\n");
+    // teste se existem tarefas
+    if(*pos == 0)
+        return SEM_TAREFAS;
+
+    // verifica se a tarefa escolhida existe
+    int pos_deletar;
+    printf("Entre com a posicao da tarefa a ser deletada: ");
+    scanf("%d", &pos_deletar);
+    pos_deletar--; // garantir posicao certa no array
+    if(pos_deletar >= *pos || pos_deletar < 0)
+        return NAO_ENCONTRADO;
+
+    for(int i = pos_deletar; i < *pos; i++){
+        tarefas[i].prioridade = tarefas[i+1].prioridade;
+        strcpy(tarefas[i].categoria, tarefas[i+1].categoria);
+        strcpy(tarefas[i].descricao,  tarefas[i+1].descricao);
+    }
+
+    *pos = *pos - 1;
+
     return OK;
 }
 
